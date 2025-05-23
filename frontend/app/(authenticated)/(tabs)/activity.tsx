eadb64fbbe060ce4293e53b6eb0eb2a7767f0f93
damijanaku <149889052+damijanaku@users.noreply.github.com>
2025-05-23 11:43:35 +0200

    MH-32 Actvity UI

--- frontend/app/(authenticated)/(tabs)/activity.tsx
+++ frontend/app/(authenticated)/(tabs)/activity.tsx
@@ -2,20 +2,38 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import  ActivityIcon  from '../../../assets/tsxicons/activitynavbaricon'
+import Value from '../../../components/Value'
+import RingProgress from '../../../components/RingProgress'
 
 
 const Activity = () => {
   return (
-    <SafeAreaView>
+
+    <SafeAreaView style={styles.container}>
       <View style={styles.sectionTitle}>
         <ActivityIcon color={'#6E49EB'} fill={'white'} />
         <Text style={styles.sectionTitleText}>Activity</Text>
+      </View>
+      <RingProgress radius={130} strokeWidth={50} progress={0.3} />
+
+      <View style={styles.values}>
+       <Value label="Steps" value="1219"></Value>
+       <Value label="Distance" value="0,75 km"></Value>
+      <Value label="Flights climbed" value="12"></Value>
+
+
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 12
+  },
   sectionTitle: { 
     flexDirection: 'row', 
     marginHorizontal: 20, 
@@ -27,6 +45,23 @@ const styles = StyleSheet.create({
     color: '#6E49EB',
     margin: 10 
   },
+  label: {
+    fontSize: 20
+    
+  },
+  values: {
+    flexDirection: 'row',
+    gap: 25,
+    flexWrap: 'wrap',
+    marginTop: 40
+  },
+  value: {
+    fontSize: 45,
+    fontWeight: 500,
+  },
+  valueContainer: {
+  }
+
 })
 
 export default Activity
