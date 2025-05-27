efa63bf072b0d1680eb796d00ba322c1cbd821dd
Maksim3l <loknarmaksim@gmail.com>
2025-05-27 21:15:03 +0200

    Minor fix

--- extensions/face_auth/API/face_auth_api.py
+++ extensions/face_auth/API/face_auth_api.py
@@ -17,7 +17,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 app = Flask(__name__)
 
 # Supabase configuration
-SUPABASE_URL = os.environ.get('SUPABASE_URL', 'http://localhost:54321')
+SUPABASE_URL = os.environ.get('SUPABASE_URL', 'http://localhost:8000')
 SUPABASE_SERVICE_KEY = os.environ.get('SUPABASE_SERVICE_KEY', 
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q')
 
